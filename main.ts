b8c9e709182df60a15a8f9f0757c2db508b293c3
Juanita <j.gila2@uniandes.edu.com>
2022-10-25 23:21:33 -0500

    .

---
+++ main.ts
@@ -0,0 +1,37 @@
+import { Serie } from "./serie";
+
+import { dataSeries } from './data';
+
+let tablaSeries = document.getElementById("id_series")!;
+let tablaPromedio = document.getElementById("promedio")!;
+
+
+function mostrarDatos(series: Serie[]): void {
+    let seriesTbody: HTMLElement = document.createElement("tbody");
+    series.forEach(Serie => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<td>${Serie.numero}</td>
+                                <td>${Serie.nombre}</td>
+                                <td>${Serie.canal}</td>
+                                <td>${Serie.temporadas}</td>
+                                <td>${Serie.resumen}</td>
+                                <td>${Serie.linkCanal}</td>
+                                <td>${Serie.imagen}</td>`;
+        seriesTbody.appendChild(trElement);
+  });
+  tablaSeries.appendChild(seriesTbody);
+}
+
+function promTempSeries(series: Serie[]): void { 
+  let numTemporadas = 0;
+  let numSeries = 0;
+  series.forEach(Serie=> {
+    numTemporadas += Serie.temporadas;
+    numSeries++;
+  })
+  let promedio: number = numTemporadas/numSeries;
+  let trElement:HTMLElement = document.createElement("tr");
+  trElement.innerHTML = `<td><b>Promedio temporadas: <b></td>
+                        <td>${promedio}</td>`;
+  tablaPromedio.appendChild(trElement);
+}
